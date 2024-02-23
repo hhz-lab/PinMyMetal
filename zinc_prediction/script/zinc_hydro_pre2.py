0ca7bf0badd22acf4dce6992e989963e90b86b87
huihui zhang <66415337+hhz-lab@users.noreply.github.com>
2024-02-23 21:59:02 +0800

    Update zinc_hydro_pre2.py

--- zinc_prediction/script/zinc_hydro_pre2.py
+++ zinc_prediction/script/zinc_hydro_pre2.py
@@ -18,7 +18,7 @@ for opt, arg in options:
 pdbid=inputid
 DBname="zinc"+str(pdbid)
 
-conn = pg.connect("dbname='"+DBname+"' user='sg_display' password='' port='5432'")
+conn = pg.connect("dbname="+DBname+" password='' port='5432'")
 cur = conn.cursor()
 
 #sql = "select distinct id,pdbid,zinc_x,zinc_y,zinc_z,chainid_a from zinc_predict_site234_2 where selected_site is true order by id "
